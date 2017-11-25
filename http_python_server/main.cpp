9553c8585283e947c29600275effb974e91b221c
Ryan (Chiungting) Lin <32078126+ryanctlin@users.noreply.github.com>
2017-11-25 14:56:57 +0000

    Update main.cpp

--- http_python_server/main.cpp
+++ http_python_server/main.cpp
@@ -17,14 +17,48 @@
 #include "http_request.h"
 
 // GLOBAL VARIABLES HERE
+C12832  lcd(PE_14, PE_12, PD_12, PD_11, PE_9);
+OdinWiFiInterface wifi;
 
+//Adding button inturrupts
+InterruptIn post_button(PF_2);
+InterruptIn get_put_button(PG_4);
+volatile bool post_clicked = false;
+volatile bool get_clicked = false;
+volatile bool put_clicked = false;
 
 // FUNCTION DEFINITIONS HERE
+void lcd_print(const char* message) {
+    lcd.cls();
+    lcd.locate(0, 3);
+    lcd.printf(message);
+}
 
+//Adding button inturrupts
+void send_post() {
+    post_clicked = true;
+}
+void send_get_put() {
+    get_clicked = true;
+}
 
 int main() {
 
     // MAIN CODE HERE
-
-
+    lcd_print("Connecting...");
+    int ret = wifi.connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
+    if (ret != 0) {
+        lcd_print("Connection error.");
+        return -1;
+    }
+    lcd_print("Successfully connected!");
+    
+    //Adding button interrups
+    post_button.rise(&send_post);
+    get_put_button.rise(&send_get_put);
+    while (true) {
+        
+    }
+}
+    
 }
