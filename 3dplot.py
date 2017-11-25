ab0256af64659123034ba2c165c8e94c92b04ad7
theo9921 <33980928+theo9921@users.noreply.github.com>
2017-11-25 22:23:40 +0000

    3d modelling
    Trying to save 3d model(plot) as a .png file

---
+++ 3dplot.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as axes3d
+
+theta, phi = np.linspace(0, 2 * np.pi, 40), np.linspace(0, np.pi, 40)
+THETA, PHI = np.meshgrid(theta, phi)
+R = np.cos(PHI**2)
+X = R * np.sin(PHI) * np.cos(THETA)
+Y = R * np.sin(PHI) * np.sin(THETA)
+Z = R * np.cos(PHI)
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1, projection='3d')
+plot = ax.plot_surface(
+    X, Y, Z, rstride=1, cstride=1, cmap=plt.get_cmap('jet'),
+    linewidth=0, antialiased=False, alpha=0.5)
+
+plt.show()
+plt.savefig('/Desktop/myplot.png')
