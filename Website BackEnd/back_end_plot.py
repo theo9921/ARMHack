d7a36bdd60191b5e4edde1942d58e73083c81b5c
theo9921 <33980928+theo9921@users.noreply.github.com>
2017-11-26 10:18:27 +0000

    BackEnd Plotting in 3D

--- Website BackEnd/back_end_plot.py
+++ Website BackEnd/back_end_plot.py
@@ -1,6 +1,9 @@
+#import urlib2
+#import time
 import numpy as np
-import urlib2
-import time
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
 #number of points
 n = 10
@@ -9,35 +12,34 @@ n = 10
 data = np.zeros((n,3))
 counter=0
 
-#read from server
-while(True):
-	f = urlib2.urlopen('http://10.25.2.118:8080')
-	inStr = f.read()
-	inStrL = inStr.split()
-	inStrF = []
-
-	print(inStrL)
-	for i in range(3):
-		inStrF.append(float(inStrL[i]))
 		
+while(counter <n):
+	X=np.random.randint(0,5)
+	Y=np.random.randint(10,15)
+	Z=np.random.randint(10,16)
+	data[counter][0] = X
+	data[counter][1] = Y
+	data[counter][2] = Z
+	counter +=1
 
-	if(counter <n):
-		X=inStrF[0]
-		Y=inStrF[1]
-		Z=inStrF[2]
-		data[counter][0] = X
-		data[counter][1] = Y
-		data[counter][2] = Z
-		counter +=1
-	else:
-		break
-	
+
 print(data)
 
+Xs = data[:,0]
+Ys = data[:,1]
+Zs = data[:,2]
 
 
+# ======
+## plot:
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+surf = ax.plot_trisurf(Xs, Ys, Zs, linewidth=0)
 
 
+fig.tight_layout()
 
-	
-	
+plt.show() # or:
+
+
