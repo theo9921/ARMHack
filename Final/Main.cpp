556c48b827a975a7348f3d81d60d14df45d1a876
Ryan (Chiungting) Lin <32078126+ryanctlin@users.noreply.github.com>
2017-11-26 10:21:44 +0000

    Create Main.cpp

---
+++ Final/Main.cpp
@@ -0,0 +1,232 @@
+//----------------------------------------------------------------------------
+// The confidential and proprietary information contained in this file may
+// only be used by a person authorised under and to the extent permitted
+// by a subsisting licensing agreement from ARM Limited or its affiliates.
+//
+// (C) COPYRIGHT 2016 ARM Limited or its affiliates.
+// ALL RIGHTS RESERVED
+//
+// This entire notice must be reproduced on all copies of this file
+// and copies of this file may only be made by a person if such person is
+// permitted to do so under the terms of a subsisting license agreement
+// from ARM Limited or its affiliates.
+//----------------------------------------------------------------------------
+#include "mbed.h"
+#include "C12832.h"
+#include "CCS811.h"
+#include "Sht31.h"
+#include "TSL2561.h"
+#include "MMA7660.h"
+#include "OdinWiFiInterface.h"
+#include "http_request.h"
+
+#include "vl53l0x_api.h"
+#include "vl53l0x_platform.h"
+#include "vl53l0x_i2c_platform.h"
+
+#define USE_I2C_2V8
+
+#include <string.h>
+#include <iomanip> // setprecision
+#include <sstream> // stringstream
+#include <stdio.h>
+
+// GLOBAL VARIABLES HERE
+std::ostringstream oss;
+MMA7660 accel(PF_0, PF_1);
+C12832  lcd(PE_14, PE_12, PD_12, PD_11, PE_9);
+OdinWiFiInterface wifi;
+
+//Adding button inturrupts
+InterruptIn post_button(PF_2);
+volatile bool post_clicked = false;
+
+// FUNCTION DEFINITIONS HERE
+
+
+void lcd_print(const char* message)
+{
+    lcd.cls();
+    lcd.locate(0, 3);
+    lcd.printf(message);
+}
+
+//LASER DISTANCE
+/*Sets up LCD and prints sensor data value of Indoor Air Quality sensor to LCD */
+
+VL53L0X_Error WaitMeasurementDataReady(VL53L0X_DEV Dev)
+{
+    VL53L0X_Error Status = VL53L0X_ERROR_NONE;
+    uint8_t NewDatReady=0;
+    uint32_t LoopNb;
+
+    if (Status == VL53L0X_ERROR_NONE) {
+        LoopNb = 0;
+        do {
+            Status = VL53L0X_GetMeasurementDataReady(Dev, &NewDatReady);
+            if ((NewDatReady == 0x01) || Status != VL53L0X_ERROR_NONE) {
+                break;
+            }
+            LoopNb = LoopNb + 1;
+            VL53L0X_PollingDelay(Dev);
+        } while (LoopNb < VL53L0X_DEFAULT_MAX_LOOP);
+
+        if (LoopNb >= VL53L0X_DEFAULT_MAX_LOOP) {
+            Status = VL53L0X_ERROR_TIME_OUT;
+        }
+    }
+
+    return Status;
+}
+
+VL53L0X_Error WaitStopCompleted(VL53L0X_DEV Dev)
+{
+    VL53L0X_Error Status = VL53L0X_ERROR_NONE;
+    uint32_t StopCompleted=0;
+    uint32_t LoopNb;
+
+    if (Status == VL53L0X_ERROR_NONE) {
+        LoopNb = 0;
+        do {
+            Status = VL53L0X_GetStopCompletedStatus(Dev, &StopCompleted);
+            if ((StopCompleted == 0x00) || Status != VL53L0X_ERROR_NONE) {
+                break;
+            }
+            LoopNb = LoopNb + 1;
+            VL53L0X_PollingDelay(Dev);
+        } while (LoopNb < VL53L0X_DEFAULT_MAX_LOOP);
+
+        if (LoopNb >= VL53L0X_DEFAULT_MAX_LOOP) {
+            Status = VL53L0X_ERROR_TIME_OUT;
+        }
+
+    }
+
+    return Status;
+}
+
+int laser()
+{
+    lcd.cls();
+
+    //Setup laser
+    int var=1, measure=0;
+    int ave=0, sum=0;
+    VL53L0X_Dev_t MyDevice;
+    VL53L0X_Dev_t *pMyDevice = &MyDevice;
+    VL53L0X_RangingMeasurementData_t    RangingMeasurementData;
+    VL53L0X_RangingMeasurementData_t   *pRangingMeasurementData    = &RangingMeasurementData;
+
+    // Initialize Comms laster
+    pMyDevice->I2cDevAddr      = 0x52;
+    pMyDevice->comms_type      =  1;
+    pMyDevice->comms_speed_khz =  400;
+
+
+    VL53L0X_RdWord(&MyDevice, VL53L0X_REG_OSC_CALIBRATE_VAL,0);
+    VL53L0X_DataInit(&MyDevice);
+    uint32_t refSpadCount;
+    uint8_t isApertureSpads;
+    uint8_t VhvSettings;
+    uint8_t PhaseCal;
+
+    VL53L0X_StaticInit(pMyDevice);
+    VL53L0X_PerformRefSpadManagement(pMyDevice, &refSpadCount, &isApertureSpads); // Device Initialization
+    VL53L0X_PerformRefCalibration(pMyDevice, &VhvSettings, &PhaseCal); // Device Initialization
+    VL53L0X_SetDeviceMode(pMyDevice, VL53L0X_DEVICEMODE_CONTINUOUS_RANGING); // Setup in single ranging mode
+    VL53L0X_SetLimitCheckValue(pMyDevice, VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, (FixPoint1616_t)(0.25*65536)); //High Accuracy mode, see API PDF
+    VL53L0X_SetLimitCheckValue(pMyDevice, VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE, (FixPoint1616_t)(18*65536)); //High Accuracy mode, see API PDF
+    VL53L0X_SetMeasurementTimingBudgetMicroSeconds(pMyDevice, 200000); //High Accuracy mode, see API PDF
+    VL53L0X_StartMeasurement(pMyDevice);
+
+    //while(1) {
+    lcd.cls();
+    lcd.locate(0,3);
+//    lcd.printf("[DISTANCE]");
+    while(var<=20) {
+        WaitMeasurementDataReady(pMyDevice);
+        VL53L0X_GetRangingMeasurementData(pMyDevice, pRangingMeasurementData);
+        measure=pRangingMeasurementData->RangeMilliMeter;
+        sum=sum+measure;
+        VL53L0X_ClearInterruptMask(pMyDevice, VL53L0X_REG_SYSTEM_INTERRUPT_GPIO_NEW_SAMPLE_READY);
+        VL53L0X_PollingDelay(pMyDevice);
+        var++;
+    }
+    ave=sum/var;
+    var=1;
+    sum=0;
+    lcd.locate(0,15);
+//    lcd.printf("%dmm", ave); // Print to LCD values
+    return ave;
+    //wait(1);
+    //}
+}
+
+//ACCELEROMETER
+void read_data()
+{
+    oss.str(std::string()); //clears stream
+    float x = accel.x();
+    float y = accel.y();
+    float z = accel.z();
+    char val[32];
+    //sprintf(val, "%.2f %.2f %.2f", x, y, z);
+//    lcd_print(val);
+    int dis = laser();
+
+    oss << x << " " << y << " " << z << " " << dis;
+}
+
+//Adding button inturrupts
+void send_post()
+{
+    post_clicked = true;
+}
+
+int main()
+{
+    // MAIN CODE HERE
+    lcd_print("Connecting...");
+    int ret = wifi.connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
+    if (ret != 0) {
+        lcd_print("Connection error.");
+        return -1;
+    }
+    lcd_print("Successfully connected!");
+
+    //Adding button interrups
+    post_button.rise(&send_post);
+
+    while (1) {
+        lcd_print("Standby");
+        while (post_clicked != 1) {
+        }
+        post_clicked = 0;
+        
+        lcd_print("Scanning");
+        while (post_clicked == 0) {
+            
+            //POST REQUESTS
+            read_data();
+
+            NetworkInterface* net = &wifi;
+            HttpRequest* request = new HttpRequest(net, HTTP_POST, "http://10.25.2.118:8080");
+
+            //Debugging purposes, displays request on lcd screen
+            //char s[64];
+//            sprintf(s, "%x", request);
+//            lcd_print(s);
+
+            request->set_header("Content-Type", "application/json");
+
+            HttpResponse* response = request->send(oss.str().c_str(), strlen(oss.str().c_str()));
+      //      lcd_print(response->get_body_as_string().c_str());
+            delete request;
+
+            wait(0.5);
+        }
+        post_clicked = 0;
+        lcd_print("Standby");
+    }
+
+}
